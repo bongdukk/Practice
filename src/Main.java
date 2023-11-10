b9f46f2e9d5e2cb965743317d7fd4d1da981d854
bongdukk <pond9762296@naer.com>
2023-11-10 20:02:19 +0900

    Commit

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello git!");
+        System.out.println("Hello git!");
+
+
 
     }
 }
